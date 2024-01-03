1c414c2f765e22cb02fc5d9b186c05ce63b5adea
hzionn <zionn98tiger@gmail.com>
2024-01-04 03:52:52 +0800

    add plotting script

---
+++ code/scripts/plot.sh
@@ -0,0 +1,11 @@
+fancplot chr2L:4mb-5mb \
+	-o theplot5.png \
+	-p triangular \
+	-vmax 2.5 \
+	output/hic/SRR5579163_B_hic.hic
+
+fancplot chr2L:0mb-15849mb \
+	-o theplot3.png \
+	-p square \
+	-vmax 0.01 \
+	output/hic/SRR5579178_10kb.hic
